e30c11f9f0c3b68f9745c779b1ff3411c288dca9
bethelmelesse <28558632+bethelmelesse@users.noreply.github.com>
2026-05-15 22:14:40 +0900

    formating change

--- src/attention_utils/multihead_attention.py
+++ src/attention_utils/multihead_attention.py
@@ -3,22 +3,28 @@ from __future__ import annotations
 import torch
 import torch.nn as nn
 
-from src.attention_utils.scaled_dot_product_attention import scaled_dot_product_attention
+from src.attention_utils.scaled_dot_product_attention import (
+    scaled_dot_product_attention,
+)
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model: int, n_heads: int, dropout: float = 0.0, is_causal: bool = False):
+    def __init__(
+        self, d_model: int, n_heads: int, dropout: float = 0.0, is_causal: bool = False
+    ):
         """Initialize the Multi-Head Attention Layer
-        
+
         Args:
             d_model (int): The dimension of the model.
             n_heads (int, optional): The number of attention heads.
             dropout (float, optional): The dropout rate. Defaults to 0.0.
-            is_causal (bool, optional): Check if the attention is causal. 
+            is_causal (bool, optional): Check if the attention is causal.
                 This is used to mask future tokens in the sequence for decoder. Defaults to False.
         """
         super().__init__()
-        assert d_model % n_heads == 0, f"d_model ({d_model}) must be divisible by n_heads ({n_heads})"
+        assert d_model % n_heads == 0, (
+            f"d_model ({d_model}) must be divisible by n_heads ({n_heads})"
+        )
 
         self.d_model = d_model
         self.n_heads = n_heads
@@ -39,9 +45,9 @@ class MultiHeadAttention(nn.Module):
             d_dim: dimension of the tensor
 
         Args:
-            query (torch.Tensor): Query tensor. 
-            key (torch.Tensor): Key tensor. 
-            value (torch.Tensor): Value tensor.  
+            query (torch.Tensor): Query tensor.
+            key (torch.Tensor): Key tensor.
+            value (torch.Tensor): Value tensor.
             attn_mask (torch.Tensor, optional): Attention mask tensor. Defaults to None.
                 This is used to mask the padding tokens in the sequence.
 
@@ -65,12 +71,23 @@ class MultiHeadAttention(nn.Module):
 
         # Apply attention - (batch_size, n_heads, q_len, head_dim)
         dropout = self.dropout if self.training else 0.0
-        attention_output = scaled_dot_product_attention(query, key, value, is_causal=self.is_causal, attn_mask=attn_mask, dropout=dropout)
+        attention_output = scaled_dot_product_attention(
+            query,
+            key,
+            value,
+            is_causal=self.is_causal,
+            attn_mask=attn_mask,
+            dropout=dropout,
+        )
 
         # Reshape for output - (batch_size, q_len, d_model)
-        attention_output = attention_output.transpose(1, 2).contiguous().view(batch_size, q_len, self.d_model)
+        attention_output = (
+            attention_output.transpose(1, 2)
+            .contiguous()
+            .view(batch_size, q_len, self.d_model)
+        )
 
-        # Output projection layer 
+        # Output projection layer
         attention_output = self.output_proj(attention_output)
 
         return attention_output
@@ -85,7 +102,9 @@ if __name__ == "__main__":
     is_causal = True
     attn_mask = None
 
-    multihead_attention = MultiHeadAttention(d_model=d_dim, n_heads=1, dropout=0.0, is_causal=is_causal)
+    multihead_attention = MultiHeadAttention(
+        d_model=d_dim, n_heads=1, dropout=0.0, is_causal=is_causal
+    )
     attention_output = multihead_attention(query, key, value, attn_mask)
     print(attention_output)
     print(attention_output.shape)
