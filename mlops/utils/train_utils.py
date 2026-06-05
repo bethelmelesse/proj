4b970b8de1983f00b6c295dd52f80a7078f35353
bethelmelesse <28558632+bethelmelesse@users.noreply.github.com>
2026-06-05 22:35:10 +0900

    indent fix

--- mlops/utils/train_utils.py
+++ mlops/utils/train_utils.py
@@ -5,8 +5,9 @@ def compute_grad_norm(model: nn.Module):
     total_norm = 0
     parameters = [p for p in model.parameters() if p.grad is not None]
 
-    for p in parameters:        param_norm = p.grad.detach().data.norm(2)  # L2 norm
-        total_norm += param_norm.item() ** 2
+    for p in parameters:
+        param_norm = p.grad.detach().data.norm(2)  # L2 norm
+    total_norm += param_norm.item() ** 2
 
     total_norm = total_norm**0.5
     return total_norm
